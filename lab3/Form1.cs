17fd0bd8fb1116beb20e527f7cc3cc570c6c9f44
Kenji P. Takehara <k.takehara.533396@umindanao.edu.ph>
2026-02-06 00:26:00 +0800

    week 3

--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -6,5 +6,45 @@ namespace lab3
 		{
 			InitializeComponent();
 		}
-	}
+
+		public class Book
+		{
+
+			public string Title { get; set; }
+			
+			public string Author { get; set; }
+
+			public virtual string GetInfo()
+			{
+				return $"{Title} by {Author}";
+			}
+        }
+
+
+        public class  Magazine : Book
+        {
+            public int IssueNumber { get; set; }	
+
+
+			public override string GetInfo()
+			{
+				return $"{Title} - Issue {IssueNumber}";
+            }
+        }
+
+
+		private void btnShowInfo_Click(object sender, EventArgs e)
+		{
+			Book myBook = new Magazine
+			{
+				Title = "Tech Weekly",
+				Author = "John Doe",
+				IssueNumber = 45
+			};
+			MessageBox.Show(myBook.GetInfo());
+		}
+
+
+
+    }
 }
